21eb7afbf8b70bf7c57f8fc8c2afb67e0aca7ffc
Kirill Sakharov <kirillsakharov@dyn3202-144.wlan.ic.ac.uk>
2023-01-25 17:41:23 +0000

    Added Frontend Code

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <mainComp />
+</template>
+
+<script>
+import mainComp from './components/mainComp.vue'
+export default {
+  name: 'App',
+  components: {
+    mainComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
